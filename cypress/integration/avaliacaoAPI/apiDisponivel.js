43688d3a41cb0a939c98f2addabb79092d7aca80
allanferre <ferreiraallan19@gmail.com>
2024-10-12 19:44:51 -0300

    deletando arquivos de testes

--- cypress/integration/avaliacaoAPI/apiDisponivel.js
+++
@@ -1,9 +0,0 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
-
-Given('que a API {string} está disponível', (apiUrl) => {
-   // Faz a requisição HTTP e verifica o status da resposta
-   cy.request(apiUrl).then((response) => {
-    // Verifica se o status da resposta é 200 (OK)
-    expect(response.status).to.eq(200)
-  })  
-});
